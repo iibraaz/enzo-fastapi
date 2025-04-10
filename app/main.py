e0f4bffb1ad8318caa29b4112b7079857b018292
iibraaz <iibraaz@icloud.com>
2025-04-10 19:40:53 +0400

    Fix package imports and structure

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.routes import router
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 
 app = FastAPI(title="Enzo API", version="1.0")
 app.include_router(router)
